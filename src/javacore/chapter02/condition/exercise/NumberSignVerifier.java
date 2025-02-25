ef192af3efecbcb7650b585504ccce1c6d2cd800
Fabien <fabien_gilles@hotmail.com>
2025-02-25 10:31:18 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter02/condition/exercise/NumberSignVerifier.java
+++ src/javacore/chapter02/condition/exercise/NumberSignVerifier.java
@@ -4,9 +4,10 @@ public class NumberSignVerifier {
 
     public static void main (String[] args) {
 
-        double number = 0.0;
+        int number = 0;
+        double number2 = 0.0;
 
-        if (number > 0.0) {
+        /*if (number > 0.0) {
             System.out.println("Le chiffre est positif !");
         }
         else if (number < 0.0) {
@@ -14,7 +15,20 @@ public class NumberSignVerifier {
         }
         else {
             System.out.println("Le chiffre est égale à 0");
+        }*/
+
+        if (number > 0) {
+            System.out.println("Le chiffre est positif");
         }
+
+        if (number == 0) {
+            System.out.println("Le chiffre est neutre");
+        }
+
+        if (number < 0) {
+            System.out.println("Le chiffre est négatif");
+        }
+
 
 
     }
