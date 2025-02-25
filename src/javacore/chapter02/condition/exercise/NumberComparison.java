421a5c9c7cecbff2af0ead81377453b7dcccb3b5
Fabien <fabien_gilles@hotmail.com>
2025-02-25 10:42:27 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

---
+++ src/javacore/chapter02/condition/exercise/NumberComparison.java
@@ -0,0 +1,22 @@
+package javacore.chapter02.condition.exercise;
+
+public class NumberComparison {
+
+    public static void main (String[] args) {
+
+        int number1 = 30;
+        int number2 = 10;
+
+        if (number1 > number2) {
+            System.out.println(number1 + " est plus grand numéro");
+        }
+        else if (number1 < number2) {
+            System.out.println("Vous n'avez pas choisi le plus grand numéro");
+        }
+        else {
+            System.out.println("Les deux numéros sont identiques");
+        }
+
+    }
+
+}
