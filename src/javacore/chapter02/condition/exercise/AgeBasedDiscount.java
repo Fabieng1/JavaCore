e276fceff078365492ac490405cad16788328dd2
Fabien <fabien_gilles@hotmail.com>
2025-02-25 18:33:26 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter02/condition/exercise/AgeBasedDiscount.java
+++ src/javacore/chapter02/condition/exercise/AgeBasedDiscount.java
@@ -4,7 +4,18 @@ public class AgeBasedDiscount {
 
     public static void main (String[] args) {
 
-        int age = 37;
+        //<=, ==, >=, > || <
+        int age = 80;
+
+        if (age <= 25) {
+            System.out.println("Vous bénéficiez du forfait jeune");
+        }
+        else if (age > 25 & age < 65) {
+            System.out.println("Vous ne bénéficiez d'aucune réduction");
+        }
+        else if (age >= 65) {
+            System.out.println("Vous bénéficiez du tarif Senior");
+        }
 
     }
 
