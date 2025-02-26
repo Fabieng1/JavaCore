ab71aabba43aa2ce1f881cf27560704d92eaaf75
Fabien <fabien_gilles@hotmail.com>
2025-02-26 21:43:54 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter02/condition/exercise/AgeBasedDiscount.java
+++ src/javacore/chapter02/condition/exercise/AgeBasedDiscount.java
@@ -5,13 +5,13 @@ public class AgeBasedDiscount {
     public static void main (String[] args) {
 
         //<=, ==, >=, > || <
-        int age = 9;
+        int age = 80;
 
         if (age <= 25) {
             System.out.println("Vous bénéficiez du forfait jeune");
         }
         //Sans doute sui-je aller trop vite en utilisant le & ?
-        else if (age > 25 & age < 65) {
+        else if (age < 65) {
             System.out.println("Vous ne bénéficiez d'aucune réduction");
         }
         else if (age >= 65) {
