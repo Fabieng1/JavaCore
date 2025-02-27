e4513f298bf512df77bca4e33c0ec7ac631c7146
Fabien <fabien_gilles@hotmail.com>
2025-02-27 15:38:08 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

---
+++ src/javacore/chapter02/condition/exercise/ScholarshipEligibility.java
@@ -0,0 +1,25 @@
+package javacore.chapter02.condition.exercise;
+
+public class ScholarshipEligibility {
+
+    public static void main (String[] args) {
+
+        // 3.5, 2,5, 2
+        float studentGpa = 3.6f;
+        // 35000, 50000, 70000
+        int houseHoldIncome = 30000;
+        // true, false
+        boolean hasExtracurricular = true;
+
+        if (studentGpa > 3.5f && houseHoldIncome < 40000 && hasExtracurricular) {
+            System.out.println("Vous êtes éligible à une bourse à taux plein");
+        }
+        else if (studentGpa > 3.5f && houseHoldIncome < 60000 && hasExtracurricular) {
+            System.out.println("Vous êtes éligible à une bourse partielle");
+        }
+        else if (studentGpa <= 3.5f && houseHoldIncome >= 60000 && !hasExtracurricular) {
+            System.out.println("Vous n'êtes pas éligible à la bourse");
+        }
+
+    }
+}
