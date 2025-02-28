b0d1e2fbd4c2155feebe8e027b074c428ab9c47b
Fabien <fabien_gilles@hotmail.com>
2025-02-28 17:46:36 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
+++ src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
@@ -5,15 +5,18 @@ public class FrenchRevenueTaxCalculator {
     public static void main (String[] args) {
 
         //10.000 1000.0 1500.0 2000.0 2500.0
-        double salary = 83000;
+        double salary = 28000;
         double abattement = salary - (salary * 10.0 / 100.0);
-        double impots = 8594.0;
-        double pourcentage = impots * 100 / abattement;
+        System.out.println("Abattement : " + abattement);
 
+        //Montant servant de base de calcul pour savoir combien d'impôts payer
         float differenceTranche2 = 29497 - 11497;
         double differenceTranche3 = 83823 - 29315;
         double differenceTranche4 = 180294 - 83823;
+        System.out.println("Montant de base : " + differenceTranche4);
 
+
+        // Impôts à payer
         double sommePourcentageTranche2 = differenceTranche2 * 11 / 100;
         double sommePourcentageTranche3 = (differenceTranche3 * 30 / 100) + sommePourcentageTranche2;
         double sommePourcentageTranche4 = (differenceTranche4 * 41 / 100) + (sommePourcentageTranche2 + sommePourcentageTranche3);
@@ -27,14 +30,15 @@ public class FrenchRevenueTaxCalculator {
 
         //Bonus
         if (salary < 11497) {
+            int somme = 0;
             System.out.println("Vous touchez moins de 11497€ par an. Vous payer donc 0€");
         }
-        else if (salary < 29315) {
+        if (salary < 29315) {
 
-            System.out.println("Total de sommePourcentageParTrance : " + sommePourcentageTranche2);
-            System.out.println("Différence tranche 2 : " + differenceTranche2 + "€");
 
-            System.out.println("Vous touchez moins de 20315€ par an. Votre pourcentage de d'impossission est de " + pourcentage + "%");
+
+
+
         }
         else if (salary < 83823) {
 
@@ -42,14 +46,10 @@ public class FrenchRevenueTaxCalculator {
             System.out.println(sommePourcentageTranche2);
 
             System.out.println("Différence tranche 3 : " + differenceTranche3 + "€");
-            System.out.println("Vous touchez moins de 83823€ par an. Votre pourcentage de d'impossission est de  " + pourcentage + "%");
         }
-        else if (salary < 180294) {
-            System.out.println("Différence tranche 3 : " + differenceTranche4 + "€");
-            System.out.println("Vous touchez moins de 180294€ par an. Votre pourcentage de d'impossission est de " + pourcentage + "%");
+        if (salary < 180294) {
         }
-        else if (salary > 180294){
-            System.out.println("Vous touchez plus de 180294€ par an. Votre pourcentage de d'impossission est de  " + pourcentage + "%");
+        if (salary > 180294){
         }
     }
 
