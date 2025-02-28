b0bf883d1b1dd46e3f891d947c4616c1b1adb062
Fabien <fabien_gilles@hotmail.com>
2025-02-28 23:12:55 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
+++ src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
@@ -5,16 +5,13 @@ public class FrenchRevenueTaxCalculator {
     public static void main (String[] args) {
 
         //10.000 1000.0 1500.0 2000.0 2500.0
-        double salary = 28000;
+        double salary = 29000;
         double abattement = salary - (salary * 10.0 / 100.0);
-        System.out.println("Abattement : " + abattement);
 
         //Montant servant de base de calcul pour savoir combien d'impôts payer
         float differenceTranche2 = 29497 - 11497;
         double differenceTranche3 = 83823 - 29315;
         double differenceTranche4 = 180294 - 83823;
-        System.out.println("Montant de base : " + differenceTranche4);
-
 
         // Impôts à payer
         double sommePourcentageTranche2 = differenceTranche2 * 11 / 100;
@@ -22,32 +19,34 @@ public class FrenchRevenueTaxCalculator {
         double sommePourcentageTranche4 = (differenceTranche4 * 41 / 100) + (sommePourcentageTranche2 + sommePourcentageTranche3);
         double sommePourcentageTranche5 = (differenceTranche4 * 45 / 100) + (sommePourcentageTranche2 + sommePourcentageTranche3 + sommePourcentageTranche4);
 
-        System.out.println(sommePourcentageTranche2);
-        System.out.println(sommePourcentageTranche3);
-        System.out.println(sommePourcentageTranche4);
-        System.out.println(sommePourcentageTranche5);
-        System.out.println("Le salaire net impossable après abattement est de : " + abattement + "€");
-
         //Bonus
         if (salary < 11497) {
             int somme = 0;
             System.out.println("Vous touchez moins de 11497€ par an. Vous payer donc 0€");
         }
         if (salary < 29315) {
-
-
-
-
-
+            double tranche2 = (29315 - 11497);
+            double sommeDePourcentageTranche2 = 11 * tranche2 / 100;
+            System.out.println("Le montant de vos impôts sur le revenu s'élève à " + sommeDePourcentageTranche2);
         }
-        else if (salary < 83823) {
+        if (salary < 83823) {
+            double tranche3 = (29315 - 11497) + (83825 - 29315);
+            double sommeDePourcentageTranche3 = 30 * tranche3 /100;
+            System.out.println("Le montant de vos impôts sur le revenu s'élève à " + sommeDePourcentageTranche3);
 
-
-            System.out.println(sommePourcentageTranche2);
-
-            System.out.println("Différence tranche 3 : " + differenceTranche3 + "€");
+            double  pourcentageDImposition3 = sommeDePourcentageTranche3 * 100 / salary;
+            System.out.println("Votre pourcentage d'imposition est de " + pourcentageDImposition3);
         }
         if (salary < 180294) {
+            double tranche4 = (29315 - 11497) + (83825 - 29315) + (180294);
+            double sommeDePourcentageTranche4 = 30 * tranche4 /100;
+            System.out.println("Le montant de vos impôts sur le revenu s'élève à " + sommeDePourcentageTranche4);
+
+            double  pourcentageDImposition4 = sommeDePourcentageTranche4 * 100 / salary;
+            System.out.println("Votre pourcentage d'imposition est de " + pourcentageDImposition4 + "%");
+            System.out.println(sommeDePourcentageTranche4);
+
+
         }
         if (salary > 180294){
         }
