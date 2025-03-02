a263a50f2b168db6d354522d5f4626634969c306
Fabien <fabien_gilles@hotmail.com>
2025-03-02 22:25:46 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
+++ src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
@@ -5,7 +5,7 @@ public class FrenchRevenueTaxCalculator {
     public static void main (String[] args) {
 
         //10.000 1000.0 1500.0 2000.0 2500.0
-        double salary = 10000;
+        double salary = 180000;
         double abattement = salary - (salary * 10.0 / 100.0);
 
         /* Les "if" n'étant plus imbriquée, ceux après celui concerné s'affiche*/
@@ -15,7 +15,7 @@ public class FrenchRevenueTaxCalculator {
             System.out.println("Vous touchez moins de 11497€ par an. Vous payer donc : " + tranche1 + "€");
         }
         if (salary < 29315) {
-            double tranche2 = (29315 - 11497);
+            double tranche2 = (salary - 11497);
             System.out.println(tranche2);
             double impotsAPayer2 = 11 * tranche2 / 100;
             System.out.println("Montant impossable : " + impotsAPayer2);
@@ -23,7 +23,7 @@ public class FrenchRevenueTaxCalculator {
             System.out.println("Le montant de vos impôts sur le revenu s'élève à " + impotsAPayer2);
         }
         if (salary < 83823) {
-            double tranche3 = (29315 - 11497) + (83825 - 29315);
+            double tranche3 = (salary - 29315) + (salary - 83825);
             double impotsAPayer3= 30 * tranche3 /100;
             System.out.println("Le montant de vos impôts sur le revenu s'élève à " + impotsAPayer3);
 
@@ -31,7 +31,7 @@ public class FrenchRevenueTaxCalculator {
             System.out.println("Votre pourcentage d'imposition est de " + pourcentageDImposition3);
         }
         if (salary < 180294) {
-            double tranche4 = (29315 - 11497) + (83825 - 29315) + (180294);
+            double tranche4 = (salary - 29315) + (salary - 83825) + (salary - 180294);
             double impotsAPayer4 = 30 * tranche4 /100;
             System.out.println("Le montant de vos impôts sur le revenu s'élève à " + impotsAPayer4);
 
