a84adaaf700aeb2a529bd0392c5946eb44cfdf3d
Fabien <fabien_gilles@hotmail.com>
2025-03-03 17:55:05 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter03/loop/exercice/FactorialCalculator.java
+++ src/javacore/chapter03/loop/exercice/FactorialCalculator.java
@@ -4,18 +4,28 @@ public class FactorialCalculator {
 
     public static void main (String[] args) {
 
+        /* Autant, j'ai compris comment calculer un factorielle,
+           autant le convertir en code, je galère
+
+         */
+
         int number = 5;
         long multiplicateur = 1;
         long result = 1;
 
         while (multiplicateur != number) {
+            System.out.println(result);
 
-            result = number * multiplicateur;
-            multiplicateur--;
+            // En mettant "result = number * multiplicateur;", "result est grisé. Bizarre
+            result = result * multiplicateur;
+            number--;
+
+
 
 
 
         }
+        System.out.println(result);
 
     }
 
