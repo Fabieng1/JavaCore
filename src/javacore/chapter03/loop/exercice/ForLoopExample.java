db745e2b9044e9edb0e83c0d003383b3f8f8f811
Fabien <fabien_gilles@hotmail.com>
2025-03-05 19:48:11 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter03/loop/exercice/ForLoopExample.java
+++ src/javacore/chapter03/loop/exercice/ForLoopExample.java
@@ -16,19 +16,25 @@ public class ForLoopExample {
             System.out.println("Les nombres décroissants sont : " + chiffresDecroissants);
         }
 
+
+        for (int nbr = 0; nbr <= 20; nbr +=2) {
+
+            System.out.println(nbr + " est pair");
+
+        }
         for (char letter = 'A'; letter <= 90; letter++) {
 
-            System.out.println(letter);
+            System.out.print(letter + " ");
         }
-
-        BigInteger nb = new BigInteger("100");
-        BigInteger result = new BigInteger("1");
+        System.out.println();
+        BigInteger nb = new BigInteger("20");
+        BigInteger result2 = new BigInteger("1");
 
 
         for (BigInteger multiplicateur = new BigInteger("1"); multiplicateur.compareTo(nb) < 1 ; multiplicateur = multiplicateur.add(BigInteger.ONE)) {
 
-            result = result.multiply(multiplicateur);
-            System.out.println(result);
+            result2 = result2.multiply(multiplicateur);
+            System.out.println(result2);
 
         }
 
