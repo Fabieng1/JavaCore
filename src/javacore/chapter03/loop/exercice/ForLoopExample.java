a06ae64c1f96367ef8e2f7251015bd4ca0ecd5d2
Fabien <fabien_gilles@hotmail.com>
2025-03-06 11:31:35 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter03/loop/exercice/ForLoopExample.java
+++ src/javacore/chapter03/loop/exercice/ForLoopExample.java
@@ -6,27 +6,38 @@ public class ForLoopExample {
 
     public static void main (String[] args) {
 
+        // Affichez les nombres de 1 à 10 : Utilisez une boucle for pour afficher les nombres de 1 à 10 dans l'ordre croissant: 1 2 3 4 5 6 7
+        // 8 9 10
         for (int chiffresCroissants = 1; chiffresCroissants <= 10; chiffresCroissants++) {
 
             System.out.println("Les nombres de 1 à 10 sont : " + chiffresCroissants);
         }
 
+        // Affichez une séquence en ordre décroissant : Utilisez une boucle for pour afficher les nombres de 10 à 1 dans l'ordre décroissant: 10 9 8 7 6 5 4 3 2 1
         for (int chiffresDecroissants = 10; chiffresDecroissants >= 1; chiffresDecroissants--) {
 
             System.out.println("Les nombres décroissants sont : " + chiffresDecroissants);
         }
 
-
+        // Affichez les nombres pairs : Utilisez une boucle for pour n'afficher que les nombres pairs entre 1 et 20, dans l'ordre croissant : 2 4 6 8 10 12 14 16 18 20 (et sans utiliser de condition !)
         for (int nbr = 0; nbr <= 20; nbr +=2) {
 
             System.out.println(nbr + " est pair");
 
         }
+
+        // Affichez l'alphabet : Utilisez une boucle for pour afficher toutes les lettres de 'A' jusque 'Z'. L'affichage devra être réaliser sur une seule et unique ligne.
+        //Exemple d'affichage : A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
         for (char letter = 'A'; letter <= 90; letter++) {
 
             System.out.print(letter + " ");
         }
+
+        //System.out.println(); pour que les nombres de commencent sur la même ligne que les noombres de la consignes suivantes
         System.out.println();
+
+        // Calculez une factorielle : Utilisez une boucle for pour calculer et afficher le résultat d'une factorielle.
+        //Exemple d'affichage : La factorielle de 4 est 24
         BigInteger nb = new BigInteger("20");
         BigInteger result2 = new BigInteger("1");
 
