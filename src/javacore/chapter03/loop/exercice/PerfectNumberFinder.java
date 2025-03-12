0a96536105ddc4c8167b6281ebed64427f56de5b
Fabien <fabien_gilles@hotmail.com>
2025-03-12 10:48:14 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter03/loop/exercice/PerfectNumberFinder.java
+++ src/javacore/chapter03/loop/exercice/PerfectNumberFinder.java
@@ -4,31 +4,34 @@ public class PerfectNumberFinder {
 
     public static void main (String[] args) {
 
-        int start = 1;
-        int end = 7;
-        int result = 0;
+        int diviseur = 1;
+        int numberEnd = 6;
+        int resultAddDivisor = 0;
 
-        while (start <= end) {
+
+        resultAddDivisor = numberEnd % diviseur;
+
+        while (diviseur < numberEnd) {
 
             // Déterminé quels sont les diviseurs de "end"
 
-            if (end % start == 0) { // Alors c'est un diviseur
+            if (numberEnd % diviseur == 0) { // Alors c'est un diviseur
 
-                result = end % start;
-                System.out.println("Modulo : " + result);
-                result = result + start;
-                System.out.println("Résultat : " + result);
+
+                System.out.println("Modulo : " + resultAddDivisor);
+                resultAddDivisor = resultAddDivisor + diviseur;
+                System.out.println("Résultat : " + resultAddDivisor);
             }
 
-            start++;
+            diviseur++;
 
         }
-        if (result == end) {
+        if (resultAddDivisor == numberEnd) {
 
-            System.out.println(end + " est un nombre parfait");
+            System.out.println(numberEnd + " est un nombre parfait");
         }
         else {
-            System.out.println(end + " n'est pas un nombre parfait parfait");
+            System.out.println(numberEnd + " n'est pas un nombre parfait");
         }
     }
 }
