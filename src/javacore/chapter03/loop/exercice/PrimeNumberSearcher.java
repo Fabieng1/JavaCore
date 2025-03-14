26dbb20086f957c2cfb789c25d3a41bac91ecc20
Fabien <fabien_gilles@hotmail.com>
2025-03-14 16:47:16 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter03/loop/exercice/PrimeNumberSearcher.java
+++ src/javacore/chapter03/loop/exercice/PrimeNumberSearcher.java
@@ -8,27 +8,21 @@ public class PrimeNumberSearcher {
 
         boolean isPrime = true;
 
-        for (int i = 2; i <= number; i++) {
-
+        for (int i = 2; i < number; i++) {
             for (int potentialDivisor = 2; i < number; potentialDivisor++) {
 
                 int reste = number % potentialDivisor;
 
                 if (reste == 0) {
                     isPrime = false;
-
                 }
-
-            }
-
-            if (isPrime == true) {
-                System.out.println(number + " est premier");
-            }
-            else {
-                System.out.println(number + " n'est pas un nombre premier");
             }
         }
-
-
+        if (isPrime == true) {
+            System.out.println(number + " est premier");
+        }
+        else {
+            System.out.println(number + " n'est pas un nombre premier");
+        }
     }
 }
