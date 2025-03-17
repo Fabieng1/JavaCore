17e87b9a92a700fff0036774e14a8513880ee443
Fabien <fabien_gilles@hotmail.com>
2025-03-17 11:02:02 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter03/loop/exercice/PrimeNumberSearcher.java
+++ src/javacore/chapter03/loop/exercice/PrimeNumberSearcher.java
@@ -4,15 +4,15 @@ public class PrimeNumberSearcher {
 
     public static void main(String[] args) {
 
-        int number = 10;
+        int number = 100;
 
         boolean isPrime = true;
 
         for (int i = 2; i < number; i++) { // Je veux aller de 2 à 100
             for (int potentialDivisor = 2; potentialDivisor <= i; potentialDivisor++) { // Tant que i est inférieur à number, on augmente de 1 le diviseur
 
-                int resultatsDivision = i / potentialDivisor;
-                System.out.println("Résultats de la division " + i + " / " + potentialDivisor + " = " + resultatsDivision);
+                int resultDivision = i / potentialDivisor;
+                System.out.println("Résultats de la division " + i + " / " + potentialDivisor + " = " + resultDivision);
                 int reste = i % potentialDivisor;
                 System.out.println("Reste de la division " + reste);
                 System.out.println();
@@ -26,9 +26,5 @@ public class PrimeNumberSearcher {
 
             System.out.println (number + " est premier");
         }
-        else {
-            System.out.println(number + " n'est pas un nombre premier");
-        }
-
     }
 }
