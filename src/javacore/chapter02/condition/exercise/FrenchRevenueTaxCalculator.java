f4e506613cbd9b2f87125a0c3b6647c838379f2d
Fabien <fabien_gilles@hotmail.com>
2025-03-20 10:59:21 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
+++ src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
@@ -5,9 +5,8 @@ public class FrenchRevenueTaxCalculator {
     public static void main (String[] args) {
 
         //10.000 1000.0 1500.0 2000.0 2500.0
-        double salaireBrutAnuelle = 29000;
-        double salaireNetAnnuelImpossable = salaireBrutAnuelle - (salaireBrutAnuelle * 25.0f / 100.0f);
-        double abattement = salaireBrutAnuelle - (salaireBrutAnuelle * 10.0 / 100.0);
+        double salaireNetImpossableAnnuelle = 83000;
+        double abattement = salaireNetImpossableAnnuelle - (salaireNetImpossableAnnuelle * 10.0 / 100.0);
 
         double impotsAPayer1 = 0;
         double impotsAPayer2 = 0;
@@ -15,30 +14,36 @@ public class FrenchRevenueTaxCalculator {
         double impotsAPayer4 = 0;
         double impotsAPayer5 = 0;
 
-        /* Les "if" n'étant plus imbriquée, ceux après celui concerné s'affiche*/
+    // Remplacement des if successif par des else if, sinon tous les if après la trancche concernée sont exécutés
 
-        if (salaireBrutAnuelle < 11497) {
+        if (salaireNetImpossableAnnuelle < 11497) {
             impotsAPayer1 = 0;
             System.out.println("Vous touchez moins de 11497€ par an. Vous payer donc : " + impotsAPayer1 + "€");
         }
-        else {
-            if (salaireBrutAnuelle < 29315.0) {
-                impotsAPayer2 = (29315.0 - 11497.0) * 11.0 / 100.0;
-                impotsAPayer2 = impotsAPayer1 + impotsAPayer2;
+        else if (salaireNetImpossableAnnuelle < 29315.0) {
 
-                System.out.println("Le montant de vos impôts sur le revenu s'élève à " + impotsAPayer2);
-            }
-            if (salaireBrutAnuelle < 83823) {
-                impotsAPayer3 = (83823. - 29315.0) * 30.0 / 100.0;
-                System.out.println(impotsAPayer3);
-            }
-            if (salaireBrutAnuelle < 180294) {
-                //impotsAPayer4 = ( 180294 - 83823) * 41.0 / 100.0;
+            impotsAPayer2 = (29315.0 - 11497.0) * 11.0 / 100.0;
+            impotsAPayer2 = impotsAPayer1 + impotsAPayer2;
 
-            }
-            if (salaireBrutAnuelle > 180294) {
+            System.out.println("Le montant de vos impôts est de " + impotsAPayer2);
+        }
+        else if (salaireNetImpossableAnnuelle < 83823) {
+            impotsAPayer3 = (83823. - 29315.0) * 30.0 / 100.0;
+            impotsAPayer3 = impotsAPayer1 + impotsAPayer2 + impotsAPayer3;
 
-            }
+            System.out.println("Le montant de vos impôts est de " + impotsAPayer3);
+        }
+        else if (salaireNetImpossableAnnuelle < 180294) {
+            impotsAPayer4 = (180294 - 83823) * 41.0 / 100.0;
+            impotsAPayer4 = impotsAPayer1 + impotsAPayer2 + impotsAPayer3 + impotsAPayer4;
+
+            System.out.println("Le montant de vos impôts est de " + impotsAPayer4);
+        }
+        else if (salaireNetImpossableAnnuelle > 180294) {
+            impotsAPayer5 = (salaireNetImpossableAnnuelle - 180294) * 45 / 100;
+            impotsAPayer5 = impotsAPayer1 + impotsAPayer2 + impotsAPayer3 + impotsAPayer4 + impotsAPayer5;
+
+            System.out.println("Le montant de vos impôts est de " + impotsAPayer5);
         }
     }
 }
