b7fdb3cd6f1c3ebfcd9cad43910fa960e742d088
Fabien <fabien_gilles@hotmail.com>
2025-03-31 15:21:03 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
+++ src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
@@ -5,7 +5,7 @@ public class FrenchRevenueTaxCalculator {
     public static void main (String[] args) {
 
         //10.000 1000.0 1500.0 2000.0 2500.0
-        double salaryAnnualNetTaxable = 30000;
+        double salaryAnnualNetTaxable = 3000000;
         double reduction = salaryAnnualNetTaxable * 10.0 / 100.0;
 
         double salaryAnnualNetTaxableAfterReduction = salaryAnnualNetTaxable - reduction;
@@ -21,27 +21,30 @@ public class FrenchRevenueTaxCalculator {
         double taxBracket4 = 0;
         double taxBracket5 = 0;
 
-
-        if (salaryAnnualNetTaxableAfterReduction < 29315.0) {
-            taxBracket2 = (salaryAnnualNetTaxableAfterReduction - 11497.0) * 11.0 / 100.0;
-            salaryAnnualNetTaxableAfterReduction = 29315.0;
-        }
-        if (salaryAnnualNetTaxableAfterReduction < 83823) {
-            taxBracket3 = (salaryAnnualNetTaxableAfterReduction - 29315.0) * 30 / 100.0;
-            salaryAnnualNetTaxableAfterReduction = salaryAnnualNetTaxable - reduction;
-        }
-        if (salaryAnnualNetTaxableAfterReduction <= 180294) {
-            taxBracket4 = (salaryAnnualNetTaxableAfterReduction - 83823.0) * 41.0 / 100.0;
-            salaryAnnualNetTaxableAfterReduction = salaryAnnualNetTaxable - reduction;
-        }
+        double remainsNetTaxable = 0;
 
         if (salaryAnnualNetTaxableAfterReduction > 180294) {
             taxBracket5 = (salaryAnnualNetTaxableAfterReduction - 180294) * 45 / 100;
-            salaryAnnualNetTaxableAfterReduction = salaryAnnualNetTaxable - reduction;
+            salaryAnnualNetTaxableAfterReduction = 180293;
+        }
+
+        if (salaryAnnualNetTaxableAfterReduction > 83823) {
+            taxBracket4 = (salaryAnnualNetTaxableAfterReduction - 83823) * 41 / 100.0;
+            salaryAnnualNetTaxableAfterReduction = 83822;
+        }
+
+        if (salaryAnnualNetTaxableAfterReduction > 29315) {
+            taxBracket3 = (salaryAnnualNetTaxableAfterReduction - 29315) * 30 / 100.0;
+            salaryAnnualNetTaxableAfterReduction = 29314;
+        }
+
+        // De 11 498 € à 29 315 € : 11 %
+        if (salaryAnnualNetTaxableAfterReduction > 11498) {
+            taxBracket2 = (salaryAnnualNetTaxableAfterReduction - 11498) * 11 / 100;
         }
 
         accumulationOfInstallmentsToBePaid = taxBracket1 + taxBracket2 + taxBracket3 + taxBracket4 + taxBracket5 ;
 
-        System.out.println(accumulationOfInstallmentsToBePaid);
+        System.out.println("Votre impôts s'élève à :  " + accumulationOfInstallmentsToBePaid);
     }
 }
