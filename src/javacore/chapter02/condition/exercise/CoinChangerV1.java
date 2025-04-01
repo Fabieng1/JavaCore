b9ed572b502472a46c8b9c61fc994df040f064c5
Fabien <fabien_gilles@hotmail.com>
2025-04-01 16:47:50 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter02/condition/exercise/CoinChangerV1.java
+++ src/javacore/chapter02/condition/exercise/CoinChangerV1.java
@@ -4,6 +4,13 @@ public class CoinChangerV1 {
 
     public static void main (String[] args) {
 
+        final int NOTE_OF_50 = 50;
+        final int NOTE_OF_20 = 20;
+        final int NOTE_OF_10 = 10;
+        final int COIN_OF_2 = 2;
+        final int COIN_OF_1 = 1;
+
+
         // Nombres à tester pour totalBill : 100, 50, 30, 10
         // Total à payer
         int totalBill = 50;
@@ -12,20 +19,31 @@ public class CoinChangerV1 {
         // Ce qu'il doit être rendu pour pas. Rendre seulement si le résultat est négatif
         int amountToReturn = totalBill - amountPaid;
 
-        // Stock dans la caisse
-        int notes50 = 2;
-        int notes20 = 5 ;
-        int notes10 = 10;
-        int coinsOf2 = 20;
-        int coinsOf1 = 30;
-
-        // Si le nombre d'amountToReturn est négatif, on fait un rendu monnaie
+        // Si le nombre d'amountToReturn est négatif, on fait un rendu monnaie. Sinon, aucun rendu monnaie n'est fait
         if (amountToReturn < 0) {
-            if (amountToReturn < 100 && notes50 > 50) {
-                notes50--;
-                amountToReturn = amountToReturn - notes50;
-
+            if (amountToReturn - NOTE_OF_50 < 0) {
+                // Je met à jour le montant à rendre
+                amountToReturn = amountToReturn - NOTE_OF_50;
+            }
+            if (amountToReturn - NOTE_OF_20 < 0) {
+                // Je met à jour le montant à rendre
+                amountToReturn = amountToReturn - NOTE_OF_20;
+            }
+            if (amountToReturn - NOTE_OF_10 < 0) {
+                // Je met à jour le montant à rendre
+                amountToReturn = amountToReturn - NOTE_OF_10;
+            }
+            if (amountToReturn - COIN_OF_2 < 0) {
+                // Je met à jour le montant à rendre
+                amountToReturn = amountToReturn - COIN_OF_2;
+            }
+            if (amountToReturn - COIN_OF_1 < 0) {
+                // Je met à jour le montant à rendre
+                amountToReturn = amountToReturn - COIN_OF_1;
             }
         }
+        int result = amountToReturn % 50;
+        System.out.println("Résultat : " + result);
+        System.out.println("Il reste à rendre : " + amountToReturn);
     }
 }
