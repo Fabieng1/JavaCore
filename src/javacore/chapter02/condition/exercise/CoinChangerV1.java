fe49f26c108b12fef6aba7b59ac119605f72319c
Fabien <fabien_gilles@hotmail.com>
2025-04-02 16:45:02 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter02/condition/exercise/CoinChangerV1.java
+++ src/javacore/chapter02/condition/exercise/CoinChangerV1.java
@@ -13,113 +13,34 @@ public class CoinChangerV1 {
 
         // Nombres à tester pour totalBill : 200, 100, 50, 30, 10
         // Total à payer
-        int totalBill = 50;
+        int totalBill = 1255;
         //Ce qu'à donner le client
-        int amountPaid = 100;
+        int amountPaid = 2000;
         // Ce qu'il doit être rendu pour pas. Rendre seulement si le résultat est négatif
         int amountToReturn = totalBill - amountPaid;
 
         // Si le nombre d'amountToReturn est négatif, on fait un rendu monnaie. Sinon, aucun rendu monnaie n'est fait
-        if (amountToReturn < 0) {
+        if (amountToReturn <= 0) {
             if (amountToReturn + NOTE_OF_50 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_50;
+
+                int nbNote50 = amountToReturn / NOTE_OF_50;
+                amountToReturn = amountToReturn - (nbNote50 * NOTE_OF_50);
             }
-            if (amountToReturn + NOTE_OF_20 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_20;
+            if (amountToReturn + NOTE_OF_20 <= 0) {
+                int nbNote20 = amountToReturn / NOTE_OF_20;
+                amountToReturn = amountToReturn - (nbNote20 * NOTE_OF_20);
             }
-            if (amountToReturn + NOTE_OF_10 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_10;
+            if (amountToReturn + NOTE_OF_10 <= 0) {
+                int nbNote10 = amountToReturn / NOTE_OF_10;
+                amountToReturn = amountToReturn - (nbNote10 * NOTE_OF_10);
             }
-            if (amountToReturn + COIN_OF_2 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + COIN_OF_2;
+            if (amountToReturn + COIN_OF_2 <= 0) {
+                int nbCoins2 = amountToReturn / COIN_OF_2;
+                amountToReturn = amountToReturn - (nbCoins2 * COIN_OF_2);
             }
-            if (amountToReturn + COIN_OF_1 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + COIN_OF_1;
-            }
-            if (amountToReturn + NOTE_OF_50 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_50;
-            }
-            if (amountToReturn + NOTE_OF_20 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_20;
-            }
-            if (amountToReturn + NOTE_OF_10 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_10;
-            }
-            if (amountToReturn + COIN_OF_2 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + COIN_OF_2;
-            }
-            if (amountToReturn + COIN_OF_1 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + COIN_OF_1;
-            }
-            if (amountToReturn + NOTE_OF_50 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_50;
-            }
-            if (amountToReturn + NOTE_OF_20 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_20;
-            }
-            if (amountToReturn + NOTE_OF_10 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_10;
-            }
-            if (amountToReturn + COIN_OF_2 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + COIN_OF_2;
-            }
-            if (amountToReturn + COIN_OF_1 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + COIN_OF_1;
-            }
-            if (amountToReturn + NOTE_OF_50 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_50;
-            }
-            if (amountToReturn + NOTE_OF_20 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_20;
-            }
-            if (amountToReturn + NOTE_OF_10 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_10;
-            }
-            if (amountToReturn + COIN_OF_2 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + COIN_OF_2;
-            }
-            if (amountToReturn + COIN_OF_1 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + COIN_OF_1;
-            }
-            if (amountToReturn + NOTE_OF_50 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_50;
-            }
-            if (amountToReturn + NOTE_OF_20 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_20;
-            }
-            if (amountToReturn + NOTE_OF_10 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + NOTE_OF_10;
-            }
-            if (amountToReturn + COIN_OF_2 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + COIN_OF_2;
-            }
-            if (amountToReturn + COIN_OF_1 < 0) {
-                // Je met à jour le montant à rendre
-                amountToReturn = amountToReturn + COIN_OF_1;
+            if (amountToReturn + COIN_OF_1 <= 0) {
+                int nbCoins1 = amountToReturn / COIN_OF_1;
+                amountToReturn = amountToReturn - (nbCoins1 * COIN_OF_1);
             }
             System.out.println("Il reste à rendre " + amountToReturn + "€");
         }
