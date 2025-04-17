db1f8100601bbaf8de866a5bdf0db0c7913b16b3
Fabien <fabien_gilles@hotmail.com>
2025-04-17 11:33:26 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter03/loop/exercise/FibonacciCalculator.java
+++ src/javacore/chapter03/loop/exercise/FibonacciCalculator.java
@@ -10,12 +10,22 @@ public class FibonacciCalculator {
         long termeN1 = 0;
         long termeN2 = 1;
 
+        long numberPair = 0;
+
         for (long compteurTermes = 1; compteurTermes <= 50; compteurTermes++) {
             long result = termeN1 + termeN2;
             termeN1 = termeN2;
             termeN2 = result;
 
             System.out.println(result);
+
+            if (result % 2 == 0) {
+
+                numberPair++;
+
+                System.out.println("Il y a " + numberPair + " nombres de Fibonacci pair");
+
+            }
 
         }
     }
