715a92eeb330494dd0296964fe6697424d795bdf
Fabien <fabien_gilles@hotmail.com>
2025-04-21 12:26:44 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter03/loop/exercise/ChessboardDrawing.java
+++ src/javacore/chapter03/loop/exercise/ChessboardDrawing.java
@@ -6,9 +6,21 @@ public class ChessboardDrawing {
 
         int chessboardSize = 5;
 
+        int countSharp = 0;
+        int countPoint = 0;
+
         for (int currentColumnsize = 1; currentColumnsize < chessboardSize; currentColumnsize++) {
-            for (int currentLinesize = 1; currentLinesize + currentColumnsize < chessboardSize; currentLinesize++) {
-                System.out.print("#" + " " + "." + " ");
+            for (int currentLinesize = 1; currentLinesize < chessboardSize; currentLinesize++) {
+
+                if (currentLinesize % 2 != 0) {
+                    System.out.print("#");
+                    System.out.print(" . ");
+                }
+                else if (currentLinesize % 2 == 0) {
+                    System.out.print(" . ");
+                    System.out.print("#");
+
+                }
             }
             System.out.println();
         }
