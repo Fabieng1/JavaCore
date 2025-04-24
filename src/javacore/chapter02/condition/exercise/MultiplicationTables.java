a711862ee3ae9cc05cd5a8eeff5b6f9aefbbe7bf
Fabien <fabien_gilles@hotmail.com>
2025-04-24 17:08:11 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

---
+++ src/javacore/chapter02/condition/exercise/MultiplicationTables.java
@@ -0,0 +1,17 @@
+package javacore.chapter02.condition.exercise;
+
+public class MultiplicationTables {
+
+    public static void main (String[] args) {
+
+        for (int tableMultiplication = 1;  tableMultiplication <= 9; tableMultiplication++) {
+            for (int multiplier = 1; multiplier <= 10; multiplier++) {
+
+                int resultMltiplication = multiplier * tableMultiplication;
+
+                System.out.println(tableMultiplication + " X " + multiplier + " = " + resultMltiplication);
+            }
+            System.out.println();
+        }
+    }
+}
