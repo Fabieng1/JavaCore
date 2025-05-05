5526e0032aae76b4bbb233dda068ad581db6a25f
Fabien <fabien_gilles@hotmail.com>
2025-05-05 12:02:38 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter03/loop/exercise/ArmstrongNumbers.java
+++ src/javacore/chapter03/loop/exercise/ArmstrongNumbers.java
@@ -6,22 +6,32 @@ public class ArmstrongNumbers {
 
     public static void main (String[] args) {
 
-        double endNumber = 10000.0;
+        double endNumber = 153.0;
+
+        double resultMultiplication = 0.0;
+        double resultAdd = 0.0;
 
 
 
-        for (double potentialNumberArmstrong = 0.0; potentialNumberArmstrong <= endNumber; potentialNumberArmstrong++) {
+        for (double potentialNumberArmstrong = 1.0; potentialNumberArmstrong <= endNumber; potentialNumberArmstrong++) {
             double resultDivision = potentialNumberArmstrong / 10.0;
             double modulo = potentialNumberArmstrong % 10.0;
 
             double digitNumber = 0.0;
 
             while (resultDivision >= 0.1) {
-                    resultDivision = resultDivision / 10;
-                    digitNumber++;
+                digitNumber++;
+                for (int digitPower = 0; digitPower < digitNumber; digitPower++) {
+                    resultMultiplication = resultMultiplication * (resultDivision);
+                }
+                resultDivision = resultDivision / 10;
             }
+
+
             System.out.println("Potentiel nombre Armstrong : " + potentialNumberArmstrong);
             System.out.println("Résultat de la division : " + resultDivision);
+            System.out.println("Résultat de la multiplication : " + resultMultiplication);
+            System.out.println("Résultat de l'addition : " + resultAdd);
             System.out.println("Quantité de chiffre par nombres : " + digitNumber);
             System.out.println("Modulo : " + modulo);
             System.out.println();
