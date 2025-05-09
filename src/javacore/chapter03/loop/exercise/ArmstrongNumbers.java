2e7ec9d841f3316107204e75a6d591fc6c4c0343
Fabien <fabien_gilles@hotmail.com>
2025-05-09 16:34:53 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter03/loop/exercise/ArmstrongNumbers.java
+++ src/javacore/chapter03/loop/exercise/ArmstrongNumbers.java
@@ -12,9 +12,10 @@ public class ArmstrongNumbers {
         int digitTwo = 0;
         int digitThree = 0;
 
-        int resultMultiplication = 0;
         int multiplicationFirst = 0;
         int multiplicationTwo = 0;
+
+        int resultAdd = 0;
 
         for (int potentialNumberArmstrong = 0; potentialNumberArmstrong <= endNumber; potentialNumberArmstrong++) {
             int digitNumber = 0;
@@ -24,37 +25,33 @@ public class ArmstrongNumbers {
 
             digitFirst = modulo;
 
-            System.out.println("Nombre Armstrong potentiel : " + potentialNumberArmstrong);
-            System.out.println("Résultat de la division dans la boucle for externe : " + resultDivision);
-            System.out.println("Modulo dans la boucle for externe : " + modulo);
-            System.out.println("DigitNumber boucle for externe : " + digitNumber);
-            System.out.println();
+            System.out.println("Potentiel nombre Armstrong : " + potentialNumberArmstrong);
+            System.out.println("digitFirst : " + digitFirst);
 
             while (resultDivision > 0) {
                 modulo = resultDivision % 10;
                 resultDivision = resultDivision / 10;
 
                 digitTwo = modulo;
+                System.out.println("digitTwo : " + digitTwo);
 
                 digitNumber++;
 
+                int resultMultiplication = digitFirst;
                 for (int digitPower = 0; digitPower < digitNumber; digitPower++) {
-                    resultMultiplication = resultMultiplication * digitFirst;
-                    resultMultiplication = resultMultiplication * digitFirst;
+                     resultMultiplication = resultMultiplication * digitFirst;
 
-                    multiplicationFirst = resultMultiplication;
-                    digitFirst = digitTwo;
+                   multiplicationFirst = resultMultiplication;
 
-                    System.out.println("Résultat MultiplicationFirst : " + multiplicationFirst);
+                   resultAdd = multiplicationFirst + multiplicationTwo;
+                    System.out.println("resultAdd : " + resultAdd);
+                    System.out.println();
+
                 }
+                System.out.println("resultMultiplication : " + resultMultiplication);
+                System.out.println("resultMultiplication digitTwo : " + resultMultiplication);
+                System.out.println("digitNumber : " + digitNumber);
 
-                System.out.println("Résultat de la division, boucle while : " + resultDivision);
-                System.out.println("Modulo de la boucle while : " + modulo);
-                System.out.println("DigitNumber de la boucle while : " + digitNumber);
-                System.out.println("Résultat Multiplication dans la boucle for interne: " + resultMultiplication);
-                System.out.println("DigitFirst : " + digitFirst);
-                System.out.println("DigitTwo : " + digitTwo);
-                System.out.println();
             }
         }
     }
