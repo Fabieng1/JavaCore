80077daedf7b7304aedc7d7624104c797323ab45
Fabien <fabien_gilles@hotmail.com>
2025-05-09 11:47:44 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter03/loop/exercise/ArmstrongNumbers.java
+++ src/javacore/chapter03/loop/exercise/ArmstrongNumbers.java
@@ -4,47 +4,56 @@ import java.util.WeakHashMap;
 
 public class ArmstrongNumbers {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        double endNumber = 153.0;
-        int sizeColumn = 7;
-        int sizeLine = 7;
+        int endNumber = 153;                ;
 
-        double resultMultiplication = 0.0;
-        double resultAdd = 0.0;
+        int digitFirst = 0;
+        int digitTwo = 0;
+        int digitThree = 0;
 
+        int resultatMultiplication = 0;
+        int multiplicationFirst = 0;
+        int multiplicationTwo = 0;
 
+        for (int potentialNumberArmstrong = 0; potentialNumberArmstrong <= endNumber; potentialNumberArmstrong++) {
+            int digitNumber = 0;
+            int resultDivision = potentialNumberArmstrong / 10;
+            int modulo = potentialNumberArmstrong % 10;
+            digitNumber++;
 
-        for (double potentialNumberArmstrong = 1.0; potentialNumberArmstrong <= endNumber; potentialNumberArmstrong++) {
-            double resultDivision = potentialNumberArmstrong / 10.0;
-            double modulo = potentialNumberArmstrong % 10.0;
+            digitFirst = modulo;
 
-            double digitNumber = 0.0;
-
-            while (resultDivision >= 0.1) {
-                digitNumber++;
-                for (int digitPower = 0; digitPower < digitNumber; digitPower++) {
-                    resultMultiplication = resultMultiplication * (resultDivision);
-                }
-                resultDivision = resultDivision / 10;
-            }
-
-            for (int column = 0; column < sizeColumn; column++) {
-                for (int line = 0; line < column; line++) {
-
-                }
-            }
-
-
-            System.out.println("Potentiel nombre Armstrong : " + potentialNumberArmstrong);
-            System.out.println("Résultat de la division : " + resultDivision);
-            System.out.println("Résultat de la multiplication : " + resultMultiplication);
-            System.out.println("Résultat de l'addition : " + resultAdd);
-            System.out.println("Quantité de chiffre par nombres : " + digitNumber);
-            System.out.println("Modulo : " + modulo);
+            System.out.println("Nombre Armstrong potentiel : " + potentialNumberArmstrong);
+            System.out.println("Résultat de la division dans la boucle for externe : " + resultDivision);
+            System.out.println("Modulo dans la boucle for externe : " + modulo);
+            System.out.println("DigitNumber boucle for externe : " + digitNumber);
             System.out.println();
+
+            while (resultDivision > 0) {
+                modulo = resultDivision % 10;
+                resultDivision = resultDivision / 10;
+
+                digitTwo = modulo;
+
+                digitNumber++;
+
+                for (int digitPower = 0; digitPower < digitNumber; digitPower++) {
+                    resultatMultiplication = resultatMultiplication * digitThree;
+                    resultatMultiplication = resultatMultiplication * modulo;
+                }
+
+                System.out.println("Résultat de la division, boucle while : " + resultDivision);
+                System.out.println("Modulo de la boucle while : " + modulo);
+                System.out.println("DigitNumber de la boucle while : " + digitNumber);
+                System.out.println("Résultat Multiplication dans la boucle for interne: " + resultatMultiplication);
+                System.out.println("DigitFirst : " + digitFirst);
+                System.out.println("DigitTwo : " + digitTwo);
+                System.out.println();
+            }
         }
     }
 }
+
 
 
--- src/javacore/chapter03/loop/exercise/Test.java
+++ src/javacore/chapter03/loop/exercise/Test.java
@@ -4,19 +4,34 @@ public class Test {
 
     public static void main (String[] args) {
 
-        for (int a = 0; a < 10; a++) {
-            System.out.println(a + " a");
-            for (int b = 0; b < 10; b++) {
-                System.out.println(b + " b");
+       int number = 1;
 
-            }
+       int resultDivision = number / 10;
+       int modulo = number % 10;
 
-            for (int c = 0; c < 10; c++) {
+        System.out.println("R1 : " + resultDivision);
+        System.out.println("M1 : " + modulo);
+        System.out.println();
 
-                System.out.println(c + " c");
+        modulo = resultDivision % 10;
+        resultDivision = resultDivision / 10;
 
-            }
-        }
+        System.out.println("R2 : " + resultDivision);
+        System.out.println("M2 : " + modulo);
+        System.out.println();
+
+        modulo = resultDivision % 10;
+        resultDivision = resultDivision / 10;
+
+        System.out.println("R3 : " + resultDivision);
+        System.out.println("M3 : " + modulo);
+        System.out.println();
+
+        modulo = resultDivision % 10;
+        resultDivision = resultDivision / 10;
+
+        System.out.println("R4 : " + resultDivision);
+        System.out.println("M4 : " + modulo);
     }
 }
 
