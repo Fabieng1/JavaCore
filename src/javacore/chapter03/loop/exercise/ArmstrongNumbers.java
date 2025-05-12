eba18941fe2aa269db6c62afa564fe6ea1660659
Fabien <fabien_gilles@hotmail.com>
2025-05-12 18:37:02 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/javacore/chapter02/condition/exercise/ParityChecker.java

--- src/javacore/chapter03/loop/exercise/ArmstrongNumbers.java
+++ src/javacore/chapter03/loop/exercise/ArmstrongNumbers.java
@@ -1,16 +1,52 @@
 package javacore.chapter03.loop.exercise;
 
-import java.util.WeakHashMap;
-
 public class ArmstrongNumbers {
 
     public static void main(String[] args) {
 
-        int endNumber = 153;                ;
+        int endNumber = 153;
+        int numberToCheck = endNumber;
 
-        int digitFirst = 0;
+        int digitNumber = 1;
+
+        int resultDivision = endNumber / 10;
+
+        while (resultDivision > 0) {
+            resultDivision = resultDivision / 10;
+            digitNumber++;
+
+        }
+        System.out.println("digitNumber : " + digitNumber);
+
+        int modulo = 153;
+
+        while (modulo > 0) {
+            modulo = numberToCheck % 10;
+            numberToCheck = numberToCheck / 10;
+
+            System.out.println("Modulo : " + modulo);
+
+
+
+
+            int resultMultiplication = modulo;
+            int resultAdd = 0;
+            for (int digitPower = 1; digitPower < digitNumber; digitPower++) {
+                resultMultiplication = resultMultiplication * modulo;
+                resultAdd = resultAdd + resultMultiplication;
+            }
+            System.out.println("Résultat Multiplication : " + resultMultiplication);
+
+
+
+            System.out.println("Addition : " + resultAdd);
+
+        }
+
+
+
+      /*  int digitFirst = 0;
         int digitTwo = 0;
-        int digitThree = 0;
 
         int multiplicationFirst = 0;
         int multiplicationTwo = 0;
@@ -33,29 +69,36 @@ public class ArmstrongNumbers {
                 resultDivision = resultDivision / 10;
 
                 digitTwo = modulo;
+                System.out.println("digitFirst : " + digitFirst);
                 System.out.println("digitTwo : " + digitTwo);
 
                 digitNumber++;
 
                 int resultMultiplication = digitFirst;
-                for (int digitPower = 0; digitPower < digitNumber; digitPower++) {
+                System.out.println("digitNumber : " + digitNumber);
+                for (int digitPower = 1; digitPower < digitNumber; digitPower++) {
                      resultMultiplication = resultMultiplication * digitFirst;
 
                    multiplicationFirst = resultMultiplication;
 
-                   resultAdd = multiplicationFirst + multiplicationTwo;
-                    System.out.println("resultAdd : " + resultAdd);
-                    System.out.println();
+                   if (digitPower == 1) {
+                       multiplicationFirst = resultMultiplication;
+                   }
+                   else if (digitPower == 2) {
+                       multiplicationTwo = resultMultiplication;
+                   }
 
+                   multiplicationFirst = multiplicationTwo;
+
+
+                   resultAdd = resultMultiplication;
+
+                   resultAdd =  multiplicationFirst + multiplicationTwo;
                 }
-                System.out.println("resultMultiplication : " + resultMultiplication);
-                System.out.println("resultMultiplication digitTwo : " + resultMultiplication);
-                System.out.println("digitNumber : " + digitNumber);
-
+                digitFirst = digitTwo;
             }
-        }
+        }*/
     }
 }
-
 
 
