ae7be95691c2793abeb015b4606cda2ff2f15fe0
Fabieng1 <fabien_gilles@hotmail.com>
2025-05-13 17:06:57 +0200

    Update ArmstrongNumbers.java

--- src/javacore/chapter03/loop/exercise/ArmstrongNumbers.java
+++ src/javacore/chapter03/loop/exercise/ArmstrongNumbers.java
@@ -7,6 +7,7 @@ public class ArmstrongNumbers {
         int endNumber = 153;
         int numberToCheck = endNumber;
 
+
         int digitNumber = 1;
 
         int resultDivision = endNumber / 10;
@@ -19,86 +20,35 @@ public class ArmstrongNumbers {
         System.out.println("digitNumber : " + digitNumber);
 
         int modulo = 153;
+        int resultAdd = 0;
 
         while (modulo > 0) {
             modulo = numberToCheck % 10;
             numberToCheck = numberToCheck / 10;
 
+            int resultMultiplication = 1;
+
             System.out.println("Modulo : " + modulo);
 
 
 
-
-            int resultMultiplication = modulo;
-            int resultAdd = 0;
-            for (int digitPower = 1; digitPower < digitNumber; digitPower++) {
+            for (int digitPower = 0; digitPower < digitNumber; digitPower++) {
                 resultMultiplication = resultMultiplication * modulo;
-                resultAdd = resultAdd + resultMultiplication;
+                System.out.println("Résultat multiplication : " + resultMultiplication);
             }
-            System.out.println("Résultat Multiplication : " + resultMultiplication);
 
 
+            resultAdd = resultAdd + resultMultiplication;
+
+
+            //System.out.println("Résultat Multiplication : " + resultMultiplication);
 
             System.out.println("Addition : " + resultAdd);
-
         }
 
+        if (resultAdd == endNumber) {
+            System.out.println(endNumber + " est un nombre Armstrong !");
+        }
 
-
-      /*  int digitFirst = 0;
-        int digitTwo = 0;
-
-        int multiplicationFirst = 0;
-        int multiplicationTwo = 0;
-
-        int resultAdd = 0;
-
-        for (int potentialNumberArmstrong = 0; potentialNumberArmstrong <= endNumber; potentialNumberArmstrong++) {
-            int digitNumber = 0;
-            int resultDivision = potentialNumberArmstrong / 10;
-            int modulo = potentialNumberArmstrong % 10;
-            digitNumber++;
-
-            digitFirst = modulo;
-
-            System.out.println("Potentiel nombre Armstrong : " + potentialNumberArmstrong);
-            System.out.println("digitFirst : " + digitFirst);
-
-            while (resultDivision > 0) {
-                modulo = resultDivision % 10;
-                resultDivision = resultDivision / 10;
-
-                digitTwo = modulo;
-                System.out.println("digitFirst : " + digitFirst);
-                System.out.println("digitTwo : " + digitTwo);
-
-                digitNumber++;
-
-                int resultMultiplication = digitFirst;
-                System.out.println("digitNumber : " + digitNumber);
-                for (int digitPower = 1; digitPower < digitNumber; digitPower++) {
-                     resultMultiplication = resultMultiplication * digitFirst;
-
-                   multiplicationFirst = resultMultiplication;
-
-                   if (digitPower == 1) {
-                       multiplicationFirst = resultMultiplication;
-                   }
-                   else if (digitPower == 2) {
-                       multiplicationTwo = resultMultiplication;
-                   }
-
-                   multiplicationFirst = multiplicationTwo;
-
-
-                   resultAdd = resultMultiplication;
-
-                   resultAdd =  multiplicationFirst + multiplicationTwo;
-                }
-                digitFirst = digitTwo;
-            }
-        }*/
     }
 }
-
-
