07ad164236b1617075a0b98a41a89a385f8f8594
Fabieng1 <fabien_gilles@hotmail.com>
2025-06-10 17:06:28 +0200

    Add files via upload

---
+++ src/javacore/chapter04/function/exercise/RecursiveFactorial.java
@@ -0,0 +1,36 @@
+package javacore.chapter04.function.exercise;
+
+public class RecursiveFactorial {
+
+    public static void main (String[] args) {
+
+        int number = 0;
+        long result = 1;
+
+        if (number == 0) {
+            System.out.println("1");
+        }
+        else {
+            result = factorialCalculator(number, result);
+            System.out.println(result);
+        }
+    }
+
+    public static long factorialCalculator (int number, long result) {
+
+        if (number < 0) {
+            System.out.println("Le nombre ne peut pas être négatif");
+        }
+        /*if (number == 0) {
+            System.out.println("1P");
+        }*/
+
+        if (number > 0) {
+            result *= number;
+            number--;
+
+            return factorialCalculator(number, result);
+        }
+        return result;
+    }
+}
