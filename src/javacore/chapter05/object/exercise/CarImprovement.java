bc56a7d761f711e82e3625c1c9c35c165cf75789
Fabien <fabien_gilles@hotmail.com>
2025-06-13 17:03:48 +0200

    Message

--- src/javacore/chapter05/object/exercise/CarImprovement.java
+++ src/javacore/chapter05/object/exercise/CarImprovement.java
@@ -9,13 +9,13 @@ public class CarImprovement {
 
         car1.run(2000);
         car1.displayInformations();
-        car1.toGo(car1.isEngineStarted);
+        car1.isEngineStarted(car1.isEngineStarted);
 
         CarImprovementObject car2 = new CarImprovementObject("FRKV515651515NKNK5541GMB455", "Ford", "Mustang", "Jaune", 120000);
 
         car2.run(4000);
         car2.displayInformations();
-        car2.toGo(car2.isEngineStarted);
+        car2.isEngineStarted(car2.isEngineStarted);
 
     }
 }
