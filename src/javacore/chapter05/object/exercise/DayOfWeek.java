de81618e27933bb17b2480288a95500d2c0bdddc
fabien <fabien_gilles@hotmail.com>
2025-06-17 12:07:42 +0200

    vrvre

---
+++ src/javacore/chapter05/object/exercise/DayOfWeek.java
@@ -0,0 +1,20 @@
+package javacore.chapter05.object.exercise;
+
+import java.util.Scanner;
+
+public class DayOfWeek {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String day;
+
+        do {
+            System.out.print("Quel jour de la semaine sommes-nous ?");
+            day = sc.nextLine();
+        } while(!day.equalsIgnoreCase("Lundi") && !day.equalsIgnoreCase("Mardi") && !day.equalsIgnoreCase("Mercredi") && !day.equalsIgnoreCase("Jeudi") && !day.equalsIgnoreCase("Vendredi") && !day.equalsIgnoreCase("SamediS") && !day.equalsIgnoreCase("Dimanche"));
+
+        System.out.println("Merci de m'avoir informer que nous étions " + day);
+    }
+}
