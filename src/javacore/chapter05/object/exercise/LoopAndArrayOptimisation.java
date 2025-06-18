490aaacd9dd67b1120301541a74ce7f4bc9c56c6
fabien <fabien_gilles@hotmail.com>
2025-06-18 14:15:48 +0200

    vrvre

---
+++ src/javacore/chapter05/object/exercise/LoopAndArrayOptimisation.java
@@ -0,0 +1,15 @@
+package javacore.chapter05.object.exercise;
+
+public class LoopAndArrayOptimisation {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        int[] results = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            results[i] = (numbers[i] * 2) + 3;
+            System.out.println("i : " + results[i]);
+        }
+    }
+}
