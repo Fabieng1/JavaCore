b9ca180e15625c7daec140e4492958882a192530
fabien <fabien_gilles@hotmail.com>
2025-06-18 14:40:29 +0200

    vrvre

---
+++ src/javacore/chapter05/object/exercise/ReversedString.java
@@ -0,0 +1,18 @@
+package javacore.chapter05.object.exercise;
+
+public class ReversedString {
+
+    public static void main(String[] args) {
+
+        String text = "! ecicrexe'l issuér zeva suov ,ovarB";
+        char newText;
+
+
+        for(int index = text.length() - 1; index > 0; index--) {
+
+            newText = text.charAt(index);
+            System.out.print(newText);
+        }
+
+    }
+}
