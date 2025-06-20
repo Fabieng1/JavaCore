4ed5f7837ec4f6214467831aaa27ab70d9cf183f
fabien <fabien_gilles@hotmail.com>
2025-06-20 22:21:43 +0200

    vrvre

---
+++ src/javacore/chapter05/object/exercise/LongestWordFinder.java
@@ -0,0 +1,24 @@
+package javacore.chapter05.object.exercise;
+
+public class LongestWordFinder {
+
+    public static void main(String[] args) {
+
+        String text = "a bb ccc ddddddddddd eeeeeeeeee fffffffff gggg hhhhh iiiiiiiiiiii jjjjjj kkkkkkk mmmmmmmm";
+
+        String[] wordReference;
+        String[] result = text.split(" ");
+
+        int counterCharacter = 0;
+
+        for (int index = 0; index < result.length; index++) {
+
+            counterCharacter++;
+
+            i
+
+        }
+
+
+    }
+}
