5683c7054186ae54155119321ed4696c180d6613
fabien <fabien_gilles@hotmail.com>
2025-06-23 17:38:09 +0200

    vrvre

--- src/javacore/chapter05/object/exercise/LongestWordFinder.java
+++ src/javacore/chapter05/object/exercise/LongestWordFinder.java
@@ -48,7 +48,7 @@ public class LongestWordFinder {
 
             result2 = result1;
         }
-
+        System.out.println();
         System.out.println("Le mot le plus long est : " + result2);
     }
 }
