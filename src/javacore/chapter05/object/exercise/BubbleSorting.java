282d2984cbf01d8db604f2d0bd4f6ab2ebef98d0
Fabien <fabien_gilles@hotmail.com>
2025-07-18 18:07:52 +0200

    Consigne 7 en cours

--- src/javacore/chapter05/object/exercise/BubbleSorting.java
+++ src/javacore/chapter05/object/exercise/BubbleSorting.java
@@ -7,14 +7,38 @@ public class BubbleSorting {
     public static void main(String[] args) {
 
         int[] numbers = {30, 25, 85, 6, 43, 58, 97, 78, 94, 27, 9, 38, 41, 35, 52, 10, 16, 22, 96, 74, 44, 55, 82, 28, 83, 69, 90, 89, 26, 7, 47, 98, 50, 42, 68, 91, 70, 65, 79, 4, 75, 49, 61, 39, 48, 72, 36, 18, 1, 15, 77, 99, 33, 24, 13, 19, 73, 17, 14, 86, 54, 45, 76, 21, 100, 63, 60, 56, 93, 87, 81, 8, 29, 34, 53, 37, 71, 67, 57, 66, 11, 46, 95, 20, 88, 64, 59, 84, 12, 92, 80, 40, 23, 3, 2, 32, 31, 5, 62, 51};
+        int[] tempTabNumber = new int[numbers.length];
+
         int numberAlone1 = 0;
         int numberAlone2 = 0;
+        int numberMax = 0;
 
-        for (int indexNumbers = 1; indexNumbers <= numbers.length; indexNumbers++) {
+        for (int indexNumbers = 1; indexNumbers < numbers.length; indexNumbers++) {
+            for (int indexNumbers2 = 1; indexNumbers2 < numbers.length; indexNumbers2++) {
+
+                if (numbers[indexNumbers2] > numberMax) {
+
+                    numberMax = numbers[indexNumbers2];
+                }
+            }
+
+            tempTabNumber[tempTabNumber.length - 1] = numberMax;
+
+            tempTabNumber[indexNumbers];
 
 
-            numberAlone1 = indexNumbers;
-            System.out.print(numberAlone1 + " ");
+            for (int indexNumbers3 = 1; indexNumbers3 < numbers.length; indexNumbers3++) {
+
+                if (numbers[indexNumbers3] == numberMax) {
+                    numberMax = 0;
+                }
+        }
+
+            tempTabNumber[tempTabNumber.length - 1] = numberMax;
+
+            numberMax = numbers[indexNumbers];
+
+            System.out.print(tempTabNumber[indexNumbers] + " ");
         }
     }
 }
