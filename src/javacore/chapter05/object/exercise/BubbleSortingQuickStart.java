1e629640dbe679fdede583ff0f33d2f56afccb71
fabien <fabien_gilles@hotmail.com>
2025-07-24 12:37:15 +0200

    En cours !

--- src/javacore/chapter05/object/exercise/BubbleSortingQuickStart.java
+++ src/javacore/chapter05/object/exercise/BubbleSortingQuickStart.java
@@ -10,33 +10,50 @@ public class BubbleSortingQuickStart {
 
         int indexTemps = 1;
         int[] numbers = {30, 25, 85, 6, 43, 58, 97, 78, 94, 27, 9, 38, 41, 35, 52, 10, 16, 22, 96, 74, 44, 55, 82, 28, 83, 69, 90, 89, 26, 7, 47, 98, 50, 42, 68, 91, 70, 65, 79, 4, 75, 49, 61, 39, 48, 72, 36, 18, 1, 15, 77, 99, 33, 24, 13, 19, 73, 17, 14, 86, 54, 45, 76, 21, 100, 63, 60, 56, 93, 87, 81, 8, 29, 34, 53, 37, 71, 67, 57, 66, 11, 46, 95, 20, 88, 64, 59, 84, 12, 92, 80, 40, 23, 3, 2, 32, 31, 5, 62, 51};
-        int[] tempsNumberDown = new int[numbers.length / 2];
-        int[] tempsNumberUp = new int[numbers.length / 2];
-        int[] tab = {};
+        int[] tempsNumbersDown = new int[1];
+        int[] tempsNumbersUp = new int[1];
 
+        int[] tempNumbers = new int[1];
+
+        int indexGenerals = 1;
         int remainder = 0;
         int remainder2 = 0;
         int indexNumbers = 0;
         int numberRandom = random.nextInt(numbers.length);
 
+        System.out.println("Number Random : " + numberRandom);
+
         for (int indexVerif = 0; indexVerif < numbers.length; indexVerif++) {
-            for (indexNumbers = 0; indexNumbers < numbers.length - 1; indexNumbers++) {
+            for (indexNumbers = 0; indexNumbers < numbers.length; indexNumbers++) {
 
-                if (numbers[indexNumbers] >= numbers[numberRandom]) {
+                if (numbers[indexNumbers] <= numbers[numberRandom]) {
 
-                    tempsNumberUp[indexNumbers - 1] = numbers[indexNumbers];
+                    tempNumbers[0] = numbers[indexTemps];
+                    tempsNumbersDown = new int[tempNumbers.length + 1];
+
+                    for (int indexDown = 0; indexDown < tempNumbers.length; indexDown++) {
+
+                        tempsNumbersDown[indexDown] = numbers[indexNumbers];
+
+                        System.out.print(tempsNumbersDown[indexDown] + " ");
+                    }
+
+                    remainder = tempsNumbersDown[indexNumbers];
+                    remainder2 = tempsNumbersDown[indexNumbers + 1];
+
+                    tempsNumbersDown[indexNumbers] = remainder2;
+                    tempsNumbersDown[indexNumbers + 1] = remainder;
+
+
                 }
-                else if (numbers[indexNumbers] < numbers[numberRandom]) {
 
-                    tempsNumberDown[indexNumbers] = numbers[indexNumbers];
-
-                }
             }
+
+
         }
 
-        for (int displayTab = 0; displayTab < numbers.length; displayTab++) {
+        /*for (int displayTab = 0; displayTab < numbers.length; displayTab++) {
             System.out.print(numbers[displayTab] + " ");
-        }
-
+        }*/
     }
 }
