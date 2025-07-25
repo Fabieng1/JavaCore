61b4f0432522fa797e4dea4418b133803fc74f3c
fabien <fabien_gilles@hotmail.com>
2025-07-25 15:58:53 +0200

    En cours !

--- src/javacore/chapter05/object/exercise/BubbleSortingQuickStart.java
+++ src/javacore/chapter05/object/exercise/BubbleSortingQuickStart.java
@@ -6,54 +6,43 @@ public class BubbleSortingQuickStart {
 
     public static void main(String[] args) {
 
-        Random random = new Random();
-
-        int indexTemps = 1;
         int[] numbers = {30, 25, 85, 6, 43, 58, 97, 78, 94, 27, 9, 38, 41, 35, 52, 10, 16, 22, 96, 74, 44, 55, 82, 28, 83, 69, 90, 89, 26, 7, 47, 98, 50, 42, 68, 91, 70, 65, 79, 4, 75, 49, 61, 39, 48, 72, 36, 18, 1, 15, 77, 99, 33, 24, 13, 19, 73, 17, 14, 86, 54, 45, 76, 21, 100, 63, 60, 56, 93, 87, 81, 8, 29, 34, 53, 37, 71, 67, 57, 66, 11, 46, 95, 20, 88, 64, 59, 84, 12, 92, 80, 40, 23, 3, 2, 32, 31, 5, 62, 51};
-        int[] tempsNumbersDown = new int[1];
-        int[] tempsNumbersUp = new int[1];
 
-        int[] tempNumbers = new int[1];
+        quickSort(numbers, 0, numbers.length - 1);
 
-        int indexGenerals = 1;
-        int remainder = 0;
-        int remainder2 = 0;
-        int indexNumbers = 0;
-        int numberRandom = random.nextInt(numbers.length);
+        // Affichage du tableau trié
+        for (int displayTab = 0; displayTab < numbers.length; displayTab++) {
+            System.out.print(numbers[displayTab] + " ");
+        }
+    }
 
-        System.out.println("Number Random : " + numberRandom);
+    public static void quickSort(int[] numbers, int indexDown, int indexUp) {
+        if (indexDown < indexUp) {
+            int indexPivot = partition(numbers, indexDown, indexUp);
+            quickSort(numbers, indexDown, indexPivot - 1); // Tri à gauche du pivot
+            quickSort(numbers, indexPivot + 1, indexUp);   // Tri à droite du pivot
+        }
+    }
 
-        for (int indexVerif = 0; indexVerif < numbers.length; indexVerif++) {
-            for (indexNumbers = 0; indexNumbers < numbers.length; indexNumbers++) {
+    public static int partition(int[] numbers, int indexDown, int indexUp) {
+        int numberRandom = numbers[indexUp]; // Pivot
+        int indexTemps = indexDown - 1;      // Indice pour les petits éléments
 
-                if (numbers[indexNumbers] <= numbers[numberRandom]) {
-
-                    tempNumbers[0] = numbers[indexTemps];
-                    tempsNumbersDown = new int[tempNumbers.length + 1];
-
-                    for (int indexDown = 0; indexDown < tempNumbers.length; indexDown++) {
-
-                        tempsNumbersDown[indexDown] = numbers[indexNumbers];
-
-                        System.out.print(tempsNumbersDown[indexDown] + " ");
-                    }
-
-                    remainder = tempsNumbersDown[indexNumbers];
-                    remainder2 = tempsNumbersDown[indexNumbers + 1];
-
-                    tempsNumbersDown[indexNumbers] = remainder2;
-                    tempsNumbersDown[indexNumbers + 1] = remainder;
-
-
-                }
-
+        for (int indexNumbers = indexDown; indexNumbers < indexUp; indexNumbers++) {
+            if (numbers[indexNumbers] <= numberRandom) {
+                indexTemps++;
+                // Échange
+                int tempNumbers = numbers[indexTemps];
+                numbers[indexTemps] = numbers[indexNumbers];
+                numbers[indexNumbers] = tempNumbers;
             }
-
-
         }
 
-        /*for (int displayTab = 0; displayTab < numbers.length; displayTab++) {
-            System.out.print(numbers[displayTab] + " ");
-        }*/
+        // Échange du pivot
+        int tempNumbers = numbers[indexTemps + 1];
+        numbers[indexTemps + 1] = numbers[indexUp];
+        numbers[indexUp] = tempNumbers;
+
+        return indexTemps + 1;
     }
 }
