33402d6140fa8a62fa4ba18be9485816adcf3eb8
fabien <fabien_gilles@hotmail.com>
2025-07-26 21:30:02 +0200

    En cours !

---
+++ src/javacore/chapter05/object/practice/PracticeStaticCar.java
@@ -0,0 +1,16 @@
+package javacore.chapter05.object.practice;
+
+public class PracticeStaticCar {
+
+    String brand;
+    String model;
+
+    double price;
+
+    public PracticeStaticCar (String brand, String model, double price) {
+
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+}
