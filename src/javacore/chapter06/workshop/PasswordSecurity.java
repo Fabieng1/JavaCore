35256c6befc676cc2e0a6046bbc1d1466856b603
Fabien <fabien_gilles@hotmail.com>
2025-12-06 13:27:29 +0100

    Consigne 3

---
+++ src/javacore/chapter06/workshop/PasswordSecurity.java
@@ -0,0 +1,9 @@
+package javacore.chapter06.workshop;
+
+public class PasswordSecurity {
+
+    public static public static void main(String[] args) {
+
+        /*Les méthodes doivent être static afin de ne pas être créer à chaque foi*/
+    }
+}
