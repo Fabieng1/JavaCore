0412de22072ba66ba3cd2009b2a17e09f39b8bf9
Fabien <fabien_gilles@hotmail.com>
2025-12-12 20:31:46 +0100

    Consigne 4 Base64

--- src/javacore/chapter06/workshop/PasswordSecurity.java
+++ src/javacore/chapter06/workshop/PasswordSecurity.java
@@ -12,32 +12,34 @@ public class PasswordSecurity {
 
         Scanner sc = new Scanner(System.in);
 
-        String encode;
-        String decode;
+        String encoding;
+        String decoding;
 
         System.out.println("Entrez votre mot de passe : ");
         String password = sc.nextLine();
 
-        encode = base64Encodage(password);
-        decode = base64Dencodage(encode);
+        encoding = base64Encoding(password);
+        decoding = base64Decoding(encoding);
 
 
-        System.out.println(encode);
-        System.out.println(decode);
+        System.out.println(encoding);
+        System.out.println(decoding);
 
 
 
     }
 
-    public static String base64Encodage (String passwordEncode) {
+    public static String base64Encoding (String passwordEncode) {
 
         return Base64.getEncoder().encodeToString(passwordEncode.getBytes(StandardCharsets.UTF_8));
     }
 
-    public static String base64Dencodage (String passwordEncode) {
+    public static String base64Decoding (String passwordEncode) {
 
         return new String(Base64.getDecoder().decode(passwordEncode));
     }
+
+
 
 
 }
